46911d327a51975ac8d4c789f6b0e302bf854573
jpalianak <100520765+jpalianak@users.noreply.github.com>
2024-03-11 22:57:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ def get_data():
   df = df.sort_values(by='Date_num')
   return df
 
-image = Image.open("logo.png")
+image = Image.open("C:/Users\juana\OneDrive\Documentos\IA\CEIA\ProyectoFinal\Imagenes/perro.jpg")
 st.sidebar.header("Company name")
 st.sidebar.image(image)
 
