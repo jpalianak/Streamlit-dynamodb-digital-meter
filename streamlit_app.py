d86c6ac6a98e90348d6a8498a841d29f358c72ae
jpalianak <100520765+jpalianak@users.noreply.github.com>
2024-03-13 13:02:15 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ with row1_col1:
   st.plotly_chart(fig)
 
 with row1_col2:
-  st.write("<h2>Maximo registro</h2>", unsafe_allow_html=True
+  st.write("<h2>Maximo registro</h2>", unsafe_allow_html=True)
   st.write("<h2>{df_orig['Value'].max())}</h2>", unsafe_allow_html=True)
   fecha_event = df_orig.loc[df_orig['Value'].idxmax(), 'Date_num']
   fecha_event = pd.to_datetime(fecha_event * 10**9)
