f28a4e3c987598c594c4a1b7d6cb4223bd33ddf5
jpalianak <100520765+jpalianak@users.noreply.github.com>
2024-03-13 13:06:19 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,8 @@ with row1_col1:
 
 with row1_col2:
   st.write("<h2>Maximo registro</h2>", unsafe_allow_html=True)
-  st.write(f'<h2>{df_orig['Value'].max())}</h2>', unsafe_allow_html=True)
+  max_event = df_orig['Value'].max())
+  st.write(f'<h2>{max_event}</h2>', unsafe_allow_html=True)
   fecha_event = df_orig.loc[df_orig['Value'].idxmax(), 'Date_num']
   fecha_event = pd.to_datetime(fecha_event * 10**9)
   st.write("<h2>Producido el</h2>", unsafe_allow_html=True)
