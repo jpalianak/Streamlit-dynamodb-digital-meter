ec99144e112fa5b6c552090d69f1d99d294d66a5
jpalianak <100520765+jpalianak@users.noreply.github.com>
2024-03-13 12:40:12 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,4 +69,6 @@ with row1_col1:
 
 with row1_col2:
   st.metric('Maximo registro',df_orig['Value'].max())
-  st.metric('Producido el', df_orig.loc[df_orig['Value'].idxmax(), 'Date_num']) 
+  fecha_event = df_orig.loc[df_orig['Value'].idxmax(), 'Date_num']
+  fecha_event = pd.to_datetime(fecha_event * 10**9)
+  st.metric('Producido el', fecha_event) 
