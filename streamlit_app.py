f68139b11c00cc9959185e7247a2e8cecdbe476a
jpalianak <100520765+jpalianak@users.noreply.github.com>
2024-03-14 12:53:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ st.sidebar.image(image)
 # Obtenemos los datos
 df_orig = get_data()
 
-row1_col1,row0_spacer, row1_col2 = st.columns((8, 0.1, 2))
+row1_col1,row0_spacer, row1_col2 = st.columns((0.75, 0.05, 0.2))
 with row1_col1:
   fig = px.line(data_frame=df_orig, x='Date', y='Value',markers=True)
   fig.update_layout(xaxis_title="Date", yaxis_title="Amper",width=1300,height=500)
