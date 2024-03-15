4003416a19f88d131b7beef6459446bfe392f42e
jpalianak <100520765+jpalianak@users.noreply.github.com>
2024-03-14 23:45:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,16 +86,17 @@ df_orig = get_data()
 
 row1_col1,row0_spacer, row1_col2 = st.columns((0.75, 0.05, 0.2))
 with row1_col1:
-  st.write('')   
-  st.write('')   
-  fig = px.line(data_frame=df_orig, x='Date', y='Value',markers=True)
-  fig.update_layout(
-    xaxis_title="Date",
-    yaxis_title="Amper",
-    width=1300,
-    height=600
-  )  
-  st.plotly_chart(fig)
+    st.write('')   
+    st.write('')   
+    with st.container(border=True)
+        fig = px.line(data_frame=df_orig, x='Date', y='Value',markers=True)
+        fig.update_layout(
+            xaxis_title="Date",
+            yaxis_title="Amper",
+            width=1300,
+            height=600
+            )
+            st.plotly_chart(fig)
 
 with row1_col2:
   st.write("<h2>Maximo registro</h2>", unsafe_allow_html=True)
