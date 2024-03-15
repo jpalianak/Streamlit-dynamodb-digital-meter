3e76af6874ff32f131699efbbabf8477550a6389
jpalianak <100520765+jpalianak@users.noreply.github.com>
2024-03-14 23:23:46 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,9 +88,8 @@ row1_col1,row0_spacer, row1_col2 = st.columns((0.75, 0.05, 0.2))
 with row1_col1:
   st.write('')   
   st.write('')   
-  st.write('')     
   fig = px.line(data_frame=df_orig, x='Date', y='Value',markers=True)
-  fig.update_layout(xaxis_title="Date", yaxis_title="Amper",width=1300,height=500)
+  fig.update_layout(xaxis_title="Date", yaxis_title="Amper",width=1300,height=700)
   st.plotly_chart(fig)
 
 with row1_col2:
