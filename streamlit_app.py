f978090938b8328d348f48c3d4593a3fa652b900
jpalianak <100520765+jpalianak@users.noreply.github.com>
2024-03-15 00:02:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,6 +97,8 @@ with row1_col1:
     st.plotly_chart(fig)
     
 with row1_col2:
+  st.write('')
+  st.write('')   
   max_event = df_orig['Value'].max()
   st.write(f'<h3><span style="font-weight: bold;">Máximo valor:</span> <span style="font-style: italic;">{max_event} Amp</span></h3>', unsafe_allow_html=True)
   fecha_event = df_orig.loc[df_orig['Value'].idxmax(), 'Date_num']
