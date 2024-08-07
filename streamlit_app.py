80792058d11afeca06277d4ec839cc6d3ecff927
jpalianak <100520765+jpalianak@users.noreply.github.com>
2024-08-07 20:14:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ st.markdown(footer,unsafe_allow_html=True)
 def get_data():
   # Crear el cliente de DynamoDB usando boto3
   dynamodb = boto3.resource('dynamodb', region_name='us-east-1')  # Reemplaza 'tu_region' con la región de tu tabla
-  table_name = 'DynamoDBTableDigital'  # Reemplaza 'nombre_de_la_tabla' con el nombre de tu tabla en DynamoDB
+  table_name = 'DynamoDBTable-SAM-Digital-Meter-OpenCV'  # Reemplaza 'nombre_de_la_tabla' con el nombre de tu tabla en DynamoDB
 
   # Obtener la tabla de DynamoDB
   table = dynamodb.Table(table_name)
