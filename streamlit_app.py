e5a0b7c1e66f8d57f4b02760f982f0b8e09d7936
jpalianak <100520765+jpalianak@users.noreply.github.com>
2024-09-10 18:42:56 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,7 +101,7 @@ fig.add_scatter(
 
 # Añadir traza para OpenCV
 fig.add_scatter(
-    x=df_orig_opencv['Date'],
+    x=df_orig_opencv['Date'] + pd.Timedelta(seconds=15),
     y=df_orig_opencv['Value']*10,
     mode='lines+markers',
     line=dict(color='red', shape='spline'),  # Color rojo para OpenCV
