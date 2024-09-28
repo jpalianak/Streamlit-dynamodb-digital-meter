94ae97efde8c4ad76fdf6031434fc52ef898387a
jpalianak <100520765+jpalianak@users.noreply.github.com>
2024-09-28 14:21:27 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -110,7 +110,7 @@ if show_cnn:
 # Añadir traza para OpenCV
 if show_opencv:
     fig.add_scatter(
-        x=df_opencv_filtered['Date'] + pd.Timedelta(seconds=6),
+        x=df_opencv_filtered['Date'] + pd.Timedelta(seconds=5),
         y=df_opencv_filtered['Value']*factor,
         mode='lines+markers',
         line=dict(color='red', shape='linear'),
