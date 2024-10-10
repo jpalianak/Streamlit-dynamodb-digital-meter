cf9f9268f2ba8d5478b28e6e712bebe9b1b29c52
jpalianak <100520765+jpalianak@users.noreply.github.com>
2024-10-10 13:23:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,12 +84,8 @@ st.sidebar.markdown("---")  # Línea divisoria para separar secciones
 start_date = datetime(2024, 10, 09) 
 
 # Filtrar los datos a partir de la fecha seleccionada
-#df_cnn_filtered = df_orig_cnn[df_orig_cnn['Date'] >= pd.to_datetime(start_date)]
-#df_opencv_filtered = df_orig_opencv[df_orig_opencv['Date'] >= pd.to_datetime(start_date)]
-
-# Convertir la fecha a solo parte de la fecha para evitar problemas de comparación con horas
-df_cnn_filtered = df_orig_cnn[df_orig_cnn['Date'] >= start_date]
-df_opencv_filtered = df_orig_opencv[df_orig_opencv['Date'] >= start_date]
+df_cnn_filtered = df_orig_cnn[df_orig_cnn['Date'] >= pd.to_datetime(start_date)]
+df_opencv_filtered = df_orig_opencv[df_orig_opencv['Date'] >= pd.to_datetime(start_date)]
 
 # Espacio entre secciones
 st.sidebar.markdown("---")  # Otra línea divisoria
